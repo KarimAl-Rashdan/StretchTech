baf26a6b5286a46689d997362b2838af3a0a1a2e
Craig Weller <crg.weller@gmail.com>
2023-02-20 16:34:52 -0700

    add initial component layout

---
+++ src/Components/PokemonMain/PokemonMain.ts
@@ -0,0 +1,38 @@
+import React from 'react'
+import './PokemonMain.css'
+
+const PokemonMain = ({ card }) => {
+
+  const pokemonList = pokemon.map(pokemon => {
+    return (
+      <div className="pokeContainer">
+        <Link to={`/${pokemon.id}`}>
+          <Movie 
+            image: { type: string} = {pokemon.image}
+            name: { type: string } = {pokemon.name}
+          />
+        </Link>
+      </div>
+    )
+  })
+
+  return (
+    <main className='main'>
+      <div> 
+      error={error} handleSearch={handleSearch} 
+      </div>
+      { searchInput ? <SearchedCards searchInput={searchInput} searchedCards={searchedCards} /> :
+        <section className="cards-display">
+          <h2>Pokemon</h2>
+          <Slider cards={cards} scroll={true} />
+        </section>
+      }
+    </main>
+  )
+}
+
+export default PokemonMain
+
+//need a card file
+//need a swiper/slider file
+//need a search file
