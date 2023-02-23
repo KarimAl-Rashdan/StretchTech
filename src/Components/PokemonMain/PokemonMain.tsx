16024ee58f9e77b64baa5a4b5450f08e925a4314
Derek Yeh <derek.yeh91@gmail.com>
2023-02-23 15:47:03 -0800

    Added comments for potential refactor

--- src/Components/PokemonMain/PokemonMain.tsx
+++ src/Components/PokemonMain/PokemonMain.tsx
@@ -33,11 +33,13 @@ const PokemonMain = ({ pokemon}: MainProps) => {
   })
 
   const handleChange = (e : InputEvent) => {
-    // setSearchInput to set the state to the input.value of the form
-    // on submit, iterate through the pokemon list and return id and set searchInput to id 
+    // this handles the event in which a user types a pokemon name in the search bar and fires everytime they type in the search bar
+    // searchInput should be updating every time a user types in the search bar and searchInput should be assigned to whatever is entered into the search bar (e.target.value)
+    // see Link notes below (button element inside <Link> tag)
     e.preventDefault();
     setSearchInput(e.target.value)
     
+    // return pokeNames = pokemon.map(mon => mon.name)
   }
 
   return (
@@ -52,6 +54,12 @@ const PokemonMain = ({ pokemon}: MainProps) => {
           <Link to={`/${searchInput}`}>
             <button type="submit">Submit</button>
           </Link>
+          {/* This <Link> tag should route the user to the url using the pokemon name they typed into the search bar
+          Error handling is still needed to ensure no routing when a pokemon is not spelled correctly */}
+          {/* ***Another option is to utilize <datalist> tag and have a list of all the pokemon that gets filtered as they type out what they want to search for. 
+            Pros: no mispelling errors
+            Cons: it will be a very long array (1008 elements) and is dependent on getting pokemon names from the api
+            - will need logic to map through all the pokemon and just return their names and store that in an array of its own to use for the <datalist>  */}
       </form>
       {/* <div> 
       error={Error} handleSearch={handleSearch} 
@@ -71,4 +79,3 @@ export default PokemonMain
 
 //need a card file
 //need a swiper/slider file
-//need a search file
