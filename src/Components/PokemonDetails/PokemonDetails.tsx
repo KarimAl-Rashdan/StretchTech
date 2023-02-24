27a1ee0296dc3539ffdada69174887b4eabb3ffc
Ryan Baer <ryanjbaer@outlook.com>
2023-02-24 16:43:48 -0700

    remove lists and restructure elements

--- src/Components/PokemonDetails/PokemonDetails.tsx
+++ src/Components/PokemonDetails/PokemonDetails.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react'
-import  fetchData from '../../ApiCalls'
+import fetchData from '../../ApiCalls'
+import './PokemonDetails.css'
 
 type PokemonDetailsProps = {
     pokemonName: string
@@ -25,40 +26,34 @@ const PokemonDetails: React.FC<PokemonDetailsProps> = ({ pokemonName }) => {
         const starterMoves = pokemon.moves.filter((thing: any) =>
             thing.version_group_details[0].move_learn_method.name === 'level-up' &&
             thing.version_group_details[0].level_learned_at === 1
-        //         versionGroupDetail.level_learned_at === 1
-                // move.version_group_details.some(
-                //     (versionGroupDetail: any) =>
-                //         versionGroupDetail.move_learn_method.name === 'level-up' &&
-                //         versionGroupDetail.level_learned_at === 1
-                // )
-            )
+        )
             .map((specificMove: any) => specificMove.move.name);
 
         return (
-            <div>
-                <div>
-                    <img src={pokemon.sprites?.other?.dream_world?.front_default} alt="" />
+            <div className='pokemon-details'>
+                <div className='image-container'>
+                    <img className='pokemon-image' src={pokemon.sprites?.other?.dream_world?.front_default} alt={`Image of ${pokemon.name}`} />
                 </div>
                 <div className="pokemon-details">
-                    <h2 className="pokemon-name">{pokemon.name}</h2>
+                    <h2 className="pokemon-name">{pokemon.name.charAt(0).toUpperCase()+ pokemon.name.slice(1)}</h2>
                     <p className="pokemon-id">ID: {pokemon.id}</p>
                     <p className="pokemon-order">Order: {pokemon.order}</p>
                     <p className="pokemon-height">Height: {pokemon.height / 10} m</p>
                     <p className="pokemon-weight">Weight: {pokemon.weight / 10} kg</p>
                     <p className="pokemon-types">
-                        Types: {pokemon.types?.map((type: any) => type.type.name).join(', ')}
+                        Types: {pokemon.types.map((type: any) => type.type.name).join(', ')}
                     </p>
                     <p className="pokemon-stats-header">Stats:</p>
-                    <ul className="pokemon-stats">
-                        {pokemon.stats?.map((stat: any) => (
-                            <li key={stat.stat.name}>
-                                <span className="stat-name">{stat.stat.name}:</span>
-                                <span className="stat-value">{stat.base_stat}</span>
-                            </li>
-                        ))}
-                    </ul>
+                    {pokemon.stats.map((stat: any) => (
+                        <div key={stat.stat.name}>
+                            <span className="stat-name">{stat.stat.name}:</span>
+                            <span className="stat-value">{stat.base_stat}</span>
+                        </div>
+                    ))}
                     <p className="pokemon-starter-move">
-                        Starter Move: {starterMoves.length > 0 ? starterMoves : 'None'}
+                        Starter Moves: {starterMoves.length > 0 ? starterMoves.map((move: any) => {
+                            return <p>{move}</p>
+                        }) : 'None'}
                     </p>
                 </div>
             </div>
