dfa4fe68de4309ca88ca224924da80e61af47b9b
karimy <karim.alrashdan14@gmail.com>
2023-02-24 09:21:20 -0700

    Update apicall and page details component

--- src/Components/PokemonMain/PokemonMain.tsx
+++ src/Components/PokemonMain/PokemonMain.tsx
@@ -1,57 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import './PokemonMain.css'
 import { Link } from 'react-router-dom'
+import fetchData from '../../ApiCalls'
 // import Card from '../Card/Card'
 
-// type MainProps = {
-//   pokemon: any,
-//   image: string,
-//   name: string,
-//   id: string,
-// }
-
-// type onePokemon = {
-//   characters: [{
-//     name: string
-//     id:number
-//     sprites : {
-//       front_default : string
-//     };
-//   }][]
-//   onePokemon: object
-// }
+type MainProps = {
+  searchName: any
+}
 
 type InputEvent = React.ChangeEvent<HTMLInputElement>
 
-const PokemonMain = () => {
+const PokemonMain = ({searchName}: MainProps) => {
 
   const [searchInput , setSearchInput] = useState("")
-  const [pokemon, setPokemon] = useState<any[]>([])
-
-  useEffect(() => {
-
-  })
-
-  // const pokeList = pokemon.map((poke: any) => {
-  //   return (
-  //     <div className="pokeContainer">
-  //       <Link to={`/${pokemon.id}`}>
-  //         <Card 
-  //           pokemon = {pokemon}
-  //           id = {pokemon.id}
-  //           image = {pokemon.image}
-  //           name = {pokemon.name}
-  //         />
-  //       </Link>
-  //     </div>
-  //   )
-  // })
-
+  
   const handleChange = (e : InputEvent) => {
     e.preventDefault();
     setSearchInput(e.target.value)
-    
-    // return pokeNames = pokemon.map(mon => mon.name)
+    searchName(searchInput)
   }
 
   return (
