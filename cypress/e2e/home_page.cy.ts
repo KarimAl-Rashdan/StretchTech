213504e104f92c849d49f470f812bd0c048c412b
Derek Yeh <derek.yeh91@gmail.com>
2023-02-25 16:29:26 -0800

    Added status code and commented dev comments

--- cypress/e2e/home_page.cy.ts
+++ cypress/e2e/home_page.cy.ts
@@ -1,17 +1,26 @@
 describe('Home Page', () => {
   beforeEach(() => {
-    cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon')
-    cy.fixture('example.json')
+    cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon/', {
+      statusCode: 200,
+    }).as('getPokemon')
+
     cy.visit('http://localhost:3000/')
+
+    // cy.wait('@getPokemon')
+
   })
 
+  // I am currently able to stub the data but I'm not entirely sure it is set up properly. All tests are passing on my end as of now. -Derek
+
   it('should have a navigation bar with a background', () => {
+
     cy.get('header')
       .should('have.css', 'background-image')
       .and('include', "http://localhost:3000/static/media/banner.52733dc59b8d8581eda9.png")
   })
 
   it('should have a title and logo', () => {
+
     cy.get('.navBar>h1')
       .contains("Gotta Catch 'Em All")
       .should('be.visible')
@@ -21,6 +30,7 @@ describe('Home Page', () => {
   })
 
   it('should have a search bar and search button', () => {
+
     cy.get('Form')
       .get('input[type="text"]')
       .get('input[placeholder="Search..."]')
@@ -32,6 +42,7 @@ describe('Home Page', () => {
   })
 
   it('should direct you to the pokemon details page when inputting a pokemon name and clicking submit', () => {
+
     cy.get('form>input')
       .type('charmander')
 
@@ -53,6 +64,11 @@ describe('Home Page', () => {
   })
 
   it('should have 5 pokemon images with their names displayed', () => {
+
+    // cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5').as('getFive')
+
+    // cy.wait("@getFive")
+
     cy.get('.card>img')
       .should('have.length', 5)
       .and('be.visible')
@@ -66,17 +82,23 @@ describe('Home Page', () => {
   })
 
   it('should direct you to the pokemon details page when clicking on the images', () => {
+
+    // cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5').as('getFive')
+
+    // cy.wait("@getFive")
+
     cy.get('.card>img')
       .first().click()
       .url().should('eq', 'http://localhost:3000/bulbasaur')
 
-    // cy.get('.card').find('img')
-      // .url().should('eq', 'http://localhost:3000/bulbasaur')
-
   })
 
   it('should direct you to the pokemon details page when clicking on another pokemon image', () => {
-    // cy.wait(2000)
+
+    // cy.intercept('GET', 'https://pokeapi.co/api/v2/pokemon/?offset=0&limit=5').as('getFive')
+
+    // cy.wait("@getFive")
+
     cy.get('.card')
       .eq(1).click()
       .url().should('eq', 'http://localhost:3000/ivysaur')
