3b1055cf168781551ff395d412f7648a21bd3188
Craig Weller <crg.weller@gmail.com>
2023-02-27 15:07:01 -0700

    add functioning go back button

--- src/Components/ErrorPage/ErrorPage.tsx
+++ src/Components/ErrorPage/ErrorPage.tsx
@@ -1,10 +1,19 @@
 import "../ErrorPage/ErrorPage.css"
 import coverImage from "../../images/PageNotFound.png"
+import { useHistory } from 'react-router-dom';
+
 
 const ErrorPage = () => {
+  const history = useHistory();
+
+  const handleGoBack = () => {
+    history.goBack();
+  };
+
   return (
     <div className="PageNotFound" style={{backgroundImage: `url(${coverImage})`} }>
-    <h2>Something went wrong: 404- Page Not Found!</h2>
+      <h2>Something went wrong: 404- Page Not Found!</h2>
+      <button className="backButton" onClick={handleGoBack}>Go Back</button>
     </div>
   )
 }
